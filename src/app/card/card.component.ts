a486d2b68053896bd4816222ff8626fa18a53beb
sanhes207 <moshenin@inbox.ru>
2020-09-21 00:18:03 +0300

    init

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Project }          fzrom '../models/project';
+import { Project }          from '../models/project';
 
 @Component({
     selector:    'app-card',
